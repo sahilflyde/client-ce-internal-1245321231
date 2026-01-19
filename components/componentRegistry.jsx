2ce6990ff1d52b3d7415ff994ddc9283a9e7ba73
Blinkflo Builder <builder@blinkflo.dev>
2026-01-19 12:05:15 +0000

    auto generated

---
+++ components/componentRegistry.jsx
@@ -0,0 +1,60 @@
+
+import AboutHeroSection from "./aboutHeroSection";
+import AboutOurStory from "./aboutOurStory";
+import HeroSection from "./heroSection";
+import MetricsSection from "./key-metric";
+import AboutOurTeam from "./aboutOurTeam";
+import TargetAudienceSectionComp from "./targetAudienceSection";
+import GridVideo from "./GridVideo";
+import TransformSection from "./TransformSection";
+import ContactSupportOptions from "./contactSupportOptions";
+import VenueStage from "./venueStage";
+import WhatWeDo from "./whatWeDo";
+import MissionSection from "./MissionSection";
+import ActiveMembers from "./activeMembers";
+import SuccessStoriesMetrics from "./SuccessStoriesMetrics";
+import PricingSection from "./PricingSection";
+import Tabs from "./tabs";
+import FeaturesSection from "./featuresSection";
+import WhyChoose from "./whyChoose";
+import FAQSection from "./FaqSection";
+import Logo from "./logo";
+import IntegrationSection from "./integrationsSection";
+import OurServices from "./ourServices";
+import SectionHeader from "./sectionHeader";
+import AboutOurValues from "./aboutOurValues";
+import CareerWhyWork from "./careerWhyWork";
+import CareersOpenPosition from "./careersOpenPosition";
+import Header from "./header";
+import Footer from "./footer";
+
+export const componentRegistry = {
+  "about-hero": AboutHeroSection,
+  "about-our-story": AboutOurStory,
+  "hero-section": HeroSection,
+  "metrics-section": MetricsSection,
+  "about-our-team": AboutOurTeam,
+  "target-audience": TargetAudienceSectionComp,
+  "grid-video": GridVideo,
+  "transform-section": TransformSection,
+  "contact-support-options": ContactSupportOptions,
+  "venue-stage": VenueStage,
+  "what-we-do": WhatWeDo,
+  "mission-section": MissionSection,
+  "active-members": ActiveMembers,
+  "trusted-section": SuccessStoriesMetrics,
+  "pricing-section": PricingSection,
+  "tabs-section": Tabs,
+  "feature-section": FeaturesSection,
+  "why-choose": WhyChoose,
+  "faq-section": FAQSection,
+  "logo-section": Logo,
+  "integration-section": IntegrationSection,
+  "our-services": OurServices,
+  "section-header": SectionHeader,
+  "about-our-values": AboutOurValues,
+  "career-why-work": CareerWhyWork,
+  "careers-open-positions": CareersOpenPosition,
+  "header-sec": Header,
+  "footer-section": Footer
+};
