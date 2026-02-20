ea8a9d5cc57e90832937f950d6d7517254b46898
Blinkflo Builder <builder@blinkflo.dev>
2026-02-20 14:36:44 +0000

    auto generated

---
+++ components/componentRegistry.jsx
@@ -0,0 +1,78 @@
+
+import TransformSection from "./TransformSection";
+import TargetAudienceSectionComp from "./targetAudienceSection";
+import AboutHeroSection from "./aboutHeroSection";
+import MissionSection from "./MissionSection";
+import AboutOurStory from "./aboutOurStory";
+import ContactSupportOptions from "./contactSupportOptions";
+import HeroSection from "./heroSection";
+import MetricSection from "./key-metric";
+import AboutOurTeam from "./aboutOurTeam";
+import GridVideo from "./GridVideo";
+import WhatWeDo from "./whatWeDo";
+import FeaturesSection from "./featuresSection";
+import WhyChoose from "./whyChoose";
+import IntegrationSection from "./integrationsSection";
+import PricingSection from "./PricingSection";
+import FaqSection from "./FaqSection";
+import Testimonial from "./testimonial";
+import ImageCard from "./card";
+import GridSection from "./GridWrapperAdvance";
+import Container from "./spacing";
+import SectionHeader from "./sectionHeader";
+import OurServices from "./ourServices";
+import ActiveMembers from "./activeMembers";
+import AboutOurValues from "./aboutOurValues";
+import AboutMetric from "./aboutMetric";
+import CareersOpenPosition from "./careersOpenPosition";
+import CareerWhyWork from "./careerWhyWork";
+import MobileItel from "./mobileItel";
+import VenueCard from "./venueStage";
+import AboutHeader from "./articleHeader";
+import Logo from "./logo";
+import Tabs from "./tabs";
+import CareersHeroSection from "./careersHeroSection";
+import TrustedSection from "./SuccessStoriesMetrics";
+import ContactOurOffices from "./ContactOurOffices";
+import Header from "./header";
+import Footer from "./footer";
+
+export const componentRegistry = {
+  "transform-section": TransformSection,
+  "target-audience": TargetAudienceSectionComp,
+  "about-hero": AboutHeroSection,
+  "mission-section": MissionSection,
+  "about-our-story": AboutOurStory,
+  "contact-support-options": ContactSupportOptions,
+  "hero-section": HeroSection,
+  "metrics-section": MetricSection,
+  "about-our-team": AboutOurTeam,
+  "grid-video": GridVideo,
+  "what-we-do": WhatWeDo,
+  "feature-section": FeaturesSection,
+  "why-choose": WhyChoose,
+  "integration-section": IntegrationSection,
+  "pricing-section": PricingSection,
+  "faq-section": FaqSection,
+  "testimonials-section": Testimonial,
+  "image-card": ImageCard,
+  "grid-section": GridSection,
+  "container-sec": Container,
+  "section-header": SectionHeader,
+  "our-services": OurServices,
+  "active-members": ActiveMembers,
+  "about-our-values": AboutOurValues,
+  "about-metric": AboutMetric,
+  "careers-open-positions": CareersOpenPosition,
+  "career-why-work": CareerWhyWork,
+  "mobile-itel": MobileItel,
+  "venue-stage": VenueCard,
+  "about-header": AboutHeader,
+  "logo-section": Logo,
+  "tabs-section": Tabs,
+  "careers-hero": CareersHeroSection,
+  "trusted-section": TrustedSection,
+  "contact-our-offices": ContactOurOffices,
+  "header-sec": Header,
+  "footer-section": Footer
+};
